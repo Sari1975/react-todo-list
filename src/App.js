95134c422b8e30e1dc41b917e21065da40135103
Sari Katariina <sarir1975@gmail.com>
2020-02-25 16:26:32 +0100

    Create a raw  todos app with arrays etcetera

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import TodoList from "./components/TodoList";
+
+class App extends Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
